b243c3f151ec5d8f0c1fca3d92152002861ec681
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-07-24 21:02:58 +0530

    Update HelloWorldSpringBootApp.java

--- HelloWorldSpringBoot/src/main/java/com/paytm/app/HelloWorldSpringBootApp.java
+++ HelloWorldSpringBoot/src/main/java/com/paytm/app/HelloWorldSpringBootApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldSpringBootApp {
 
 		public static void main(String[] args){
-			SpringApplication.run(HelloWorldSpringBootApp.class,args);
+			SpringApplication.run(HelloWorldSpringBootApp.class,args); 
 		}
 }
